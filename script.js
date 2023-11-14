2c982edd0de53ba93d4adda72412146adae66657
David Agredano <david.agredano@gmail.com>
2023-11-14 20:33:13 +0100

    Add main functionality

--- script.js
+++ script.js
@@ -1,24 +1,89 @@
+let storedNumber;
+let operator;
+
 let display = {
   value: "",
   DOMElement: document.getElementById("display"),
-  update(str) {
+  append(str) {
     this.value += str;
+    this.update();
+  },
+  replace(str) {
+    this.value = str;
+    this.update();
+  },
+  update() {
     this.DOMElement.innerText = this.value;
+  },
+  clearValue() {
+    this.value = "";
   },
 };
 
-let firstNumber;
-let operator;
-let secondNumber;
+const displayValue = display.value;
+console.table({ storedNumber, operator, displayValue });
 
 // Button click handlers
 const buttons = document.querySelector("#buttons");
 buttons.addEventListener("click", (e) => {
   const isNumberButton = e.target.classList.contains("number");
-  if (isNumberButton) {
-    const buttonValue = e.target.innerText;
-    display.update(buttonValue);
+  const isOperatorButton = e.target.classList.contains("operator");
+  const isEqualsButton = e.target.id === "equals";
+  const isClearButton = e.target.id === "clear";
+  console.log(isClearButton);
+  if (isClearButton) {
+    display.replace("");
+    storedNumber = null;
+    operator = null;
+  } else if (isNumberButton) {
+    const numberClicked = e.target.innerText;
+    display.append(numberClicked);
+  } else if (isOperatorButton) {
+    if (!operator) {
+      storedNumber = Number(display.value);
+      operator = e.target.innerText;
+      display.clearValue();
+    } else if (operator) {
+      // Handle divide by 0 error
+      if (operator === "/" && display.value === "0") {
+        alert("#DIV/0! error");
+        display.replace("");
+        storedNumber = null;
+        operator = null;
+        return;
+      }
+      // show the result
+      const result = operate(operator, storedNumber, Number(display.value));
+      display.replace(hasLongDecimals(result) ? result.toFixed(5) : result);
+      console.table({ storedNumber, operator });
+
+      // reset variables
+      storedNumber = result;
+      operator = e.target.innerText;
+      display.clearValue();
+    }
+  } else if (isEqualsButton) {
+    if (storedNumber && operator && display.value) {
+      if (operator === "/" && display.value === "0") {
+        // Handle divide by 0 error
+        alert("#DIV/0! error");
+        display.replace("");
+        storedNumber = null;
+        operator = null;
+        return;
+      }
+
+    // show the result
+    const result = operate(operator, storedNumber, Number(display.value));
+    display.replace(hasLongDecimals(result) ? result.toFixed(5) : result);
+
+    // reset variables
+    storedNumber = result;
+    operator = null;
+    }
   }
+  const displayValue = display.value;
+  console.table({ storedNumber, operator, displayValue });
 });
 
 // Operations
@@ -27,4 +92,13 @@ function operate(operator, a, b) {
   else if (operator === "-") return a - b;
   else if (operator === "*") return a * b;
   else if (operator === "/") return a / b;
+}
+
+// Helpers
+function hasLongDecimals(num) {
+  sumString = num.toString();
+  var decimalIndex = sumString.indexOf(".");
+  return decimalIndex === -1
+    ? false
+    : sumString.slice(decimalIndex + 1).length > 5;
 }
